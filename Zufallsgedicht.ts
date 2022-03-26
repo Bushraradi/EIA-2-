f427e196aab9d41958340173870631f489197f2f
Bushra Radi <bushra.radi@hs-furtwangen.de>
2022-03-26 21:30:44 +0100

    A01

---
+++ Zufallsgedicht.ts
@@ -0,0 +1,7 @@
+namespace A01
+
+let subjekte: string[] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+
+let prädikate: string[] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+
+let objekte: string[] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
