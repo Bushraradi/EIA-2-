e0ad5ef1a0a0a0af3b5855a39e570b19d6f083af
Bushra Radi <bushra.radi@hs-furtwangen.de>
2022-03-26 21:42:35 +0100

    A1

--- Zufallsgedicht.ts
+++ Zufallsgedicht.ts
@@ -5,3 +5,36 @@ let subjekte: string[] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbled
 let prädikate: string[] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
 
 let objekte: string[] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+
+for (let index: number = subjekte.length; index > 0; index--) {
+       
+    
+    let resultat: string = getVerse(subjekte, prädikate, objekte);
+
+    console.log(resultat);
+
+}
+
+
+function getVerse (_Subjekte: string[], _Prädikate: string[], _Objekte: string[]): string {
+
+    let zeile: string = " ";
+
+    let randomnumber1: number = Math.floor(Math.random() * _Subjekte.length);
+
+    zeile = zeile + _Subjekte.splice(randomnumber1, 1)[0] + " ";
+
+    let randomnumber2: number = Math.floor(Math.random() * _Prädikate.length);
+
+    zeile = zeile + _Prädikate.splice(randomnumber2, 1)[0] + " ";
+
+    let randomnumber3: number = Math.floor(Math.random() * _Objekte.length);
+
+    zeile = zeile + _Objekte.splice(randomnumber3, 1)[0] + " ";
+
+    return zeile;
+
+
+
+}
+
