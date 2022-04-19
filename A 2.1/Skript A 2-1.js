6e7ad9d04079a06c290c75496342e3f54f114fab
Bushra Radi <bushra.radi@hs-furtwangen.de>
2022-04-19 12:40:55 +0200

    script

--- A 2.1/Skript A 2-1.js
+++ A 2.1/Skript A 2-1.js
@@ -1,10 +1,14 @@
 var Eventinspector;
 (function (Eventinspector) {
     window.addEventListener("load", handleLoad);
+    // tslint:disable-next-line: typedef
     function handleLoad(_event) {
         //*Event-Listener und Body key-up-Listener auf dem Body und den divs liegen
+        // tslint:disable-next-line: typedef
         var body = document.body;
+        // tslint:disable-next-line: typedef
         var div0 = document.getElementById("div0");
+        // tslint:disable-next-line: typedef
         var div1 = document.getElementById("div1");
         body.addEventListener("click", logInfo);
         div0.addEventListener("click", logInfo);
@@ -16,16 +20,23 @@ var Eventinspector;
         document.addEventListener("mosemove", setInfoBox);
         document.addEventListener("keyup", logInfo);
     }
+    // tslint:disable-next-line: typedef
     function setInfoBox(_event) {
+        // tslint:disable-next-line: typedef
         var x = _event.offsetX;
+        // tslint:disable-next-line: typedef
         var y = _event.offsetY;
+        // tslint:disable-next-line: typedef
         var target = _event.target;
+        // tslint:disable-next-line: typedef
         var span = document.getElementById("span");
+        // tslint:disable-next-line: typedef
         var mousepos = "X: " + x + "px " + "Y: " + y + "px ";
         span.innerHTML = mousepos + target;
         span.style.left = x + "px";
         span.style.top = y + "px";
     }
+    // tslint:disable-next-line: typedef
     function logInfo(_event) {
         console.log(_event);
         console.log(_event.type);
--- A 2.1/Skript A 2-1.ts
+++ A 2.1/Skript A 2-1.ts
@@ -1,10 +1,14 @@
 namespace Eventinspector {
 
 window.addEventListener("load", handleLoad);
+// tslint:disable-next-line: typedef
 function handleLoad (_event): void {
 //*Event-Listener und Body key-up-Listener auf dem Body und den divs liegen
+// tslint:disable-next-line: typedef
 let body = document.body;
+// tslint:disable-next-line: typedef
 let div0 = document.getElementById ("div0");
+// tslint:disable-next-line: typedef
 let div1 = document.getElementById ("div1");
 
 body.addEventListener ("click", logInfo);
@@ -19,20 +23,27 @@ document.addEventListener("click", logInfo);
 document.addEventListener("mosemove", setInfoBox);
 document.addEventListener("keyup", logInfo);
 
- }
+}
 
-function setInfoBox (_event): void {
+// tslint:disable-next-line: typedef
+function setInfoBox (_event) {
+// tslint:disable-next-line: typedef
 let x = _event.offsetX;
+// tslint:disable-next-line: typedef
 let y = _event.offsetY;
+// tslint:disable-next-line: typedef
 let target = _event.target;
-let span = document.getElementById ("span");
+// tslint:disable-next-line: typedef
+let span = document.getElementById("span");
+// tslint:disable-next-line: typedef
 let mousepos = "X: " + x + "px " + "Y: " + y + "px ";
 span.innerHTML = mousepos + target;
 span.style.left = x + "px";
 span.style.top = y + "px";
 
 }
-function logInfo (_event): void {
+// tslint:disable-next-line: typedef
+function logInfo (_event) {
 console.log(_event);
 console.log(_event.type);
 console.log(_event.target);
