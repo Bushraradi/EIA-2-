cce8fd720c043ed0bccb996284f81b74b675b1c9
Bushra Radi <bushra.radi@hs-furtwangen.de>
2022-04-19 09:51:48 +0200

    skript update

--- A 2.1/Skript A 2-1.js
+++ A 2.1/Skript A 2-1.js
@@ -1,62 +1,36 @@
 var Eventinspector;
 (function (Eventinspector) {
     window.addEventListener("load, handleLoad");
-    void {
-        function: handleLoad(_event), void: {
-            //*Event-Listener und Body key-up-Listener auf dem Body und den divs liegen
-            let: let,
-            body: body,
-            let: let,
-            div0: div0,
-            let: let,
-            div0: div0,
-            body: body,
-            : .addEventListener("click, logInfo"),
-            div0: div0,
-            : .addEventListener("click, logInfo"),
-            div1: div1,
-            : .addEventListener("click, logInfo"),
-            body: body,
-            : .addEventListener("keyup, logInfo"),
-            div0: div0,
-            : .addEventListener("keyup, logInfo"),
-            div1: div1,
-            : .addEventListener("keyup, logInfo"),
-            document: document,
-            : .addEventListener("click", logInfo),
-            document: document,
-            : .addEventListener("mosemove", setInfoBox),
-            document: document,
-            : .addEventListener("keyup", logInfo)
-        },
-        function: SetInfoBox(_event), void: {
-            let: let,
-            x: x,
-            let: let,
-            y: y,
-            let: let,
-            target: target,
-            let: let,
-            span: span,
-            span: span,
-            : .innerHTML = mouseposition + target,
-            let: let,
-            mouseposition: mouseposition,
-            span: span,
-            : .style.left = x + "px",
-            span: span,
-            : .style.top = y + "px"
-        },
-        function: logInfo(_event), void: {
-            console: console,
-            : .log(_event),
-            console: console,
-            : .log(_event.type),
-            console: console,
-            : .log(_event.target),
-            console: console,
-            : .log(_event.currenttTarget)
-        }
-    };
+    function handleLoad(_event) {
+        //*Event-Listener und Body key-up-Listener auf dem Body und den divs liegen
+        var body = document.body;
+        var div0 = document.getElementById("div0");
+        var div0 = document.getElementById("div1");
+        body.addEventListener("click, logInfo");
+        div0.addEventListener("click, logInfo");
+        div1.addEventListener("click, logInfo"),
+            body.addEventListener("keyup, logInfo");
+        div0.addEventListener("keyup, logInfo");
+        div1.addEventListener("keyup, logInfo");
+        document.addEventListener("click", logInfo);
+        document.addEventListener("mosemove", setInfoBox);
+        document.addEventListener("keyup", logInfo);
+    }
+    function SetInfoBox(_event) {
+        var x = _event.offsetX;
+        var y = _event.offsetY;
+        var target = _event.target;
+        var span = document.getElementById("span");
+        span.innerHTML = mouseposition + target;
+        var mouseposition = "X: " + x + "px " + "Y: " + y + "px ";
+        span.style.left = x + "px";
+        span.style.top = y + "px";
+    }
+    function logInfo(_event) {
+        console.log(_event);
+        console.log(_event.type);
+        console.log(_event.target);
+        console.log(_event.currenttTarget);
+    }
 })(Eventinspector || (Eventinspector = {}));
 //# sourceMappingURL=Skript%20A%202-1.js.map
